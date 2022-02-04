b290eecf60e750614bf0614c3121c39cb897a3ad
Abhisek Samal <92312713+CodeAvk@users.noreply.github.com>
2022-02-05 01:16:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 
 def send_msg(number,message):
     url='https://www.fast2sms.com/dev/bulkV2 '
+    
+    # Creating Dictinoary
     params={
         "authorization":"5yMPCmJIxOLtF49EvD0gbYeswNZq2ncp8QiVRHTdulKhAfBzkW4bEh7pWKz9RSsaf83AUeNj1HBdwTZc",
         "sender_id":"TXTIND", # the sender id may be vary...so make sure that you use correct sender id and you can customize it
@@ -12,6 +14,7 @@ def send_msg(number,message):
         "numbers":number
 
     }
+    # Get Request
     response=requests.get(url,params=params)
     dic=response.json()
     print(dic)
